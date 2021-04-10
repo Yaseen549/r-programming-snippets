472f2fc4085a70554d0203dd1db1b14fc205ce4d
Yaseen549 <fantasticyaseenshariff@gmail.com>
2021-04-10 16:32:06 +0530

    Initial Commit

---
+++ R_Programming/R Basics-1 - Factors and Levels.R
@@ -0,0 +1,6 @@
+# 1. Create a vector variable with the values "yes", "no", "maybe".
+# 2. Display the levels in this variable.
+
+vector_variable <- c('yes','no','yes','maybe')
+v_variable <- factor(vector_variable)
+print(v_variable)
